6295f2884d0fa5566ba2bf290d85ce64e2e8fdd1
TyWang1990 <111158867+TyWang1990@users.noreply.github.com>
2023-06-15 20:16:30 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,38 +26,80 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-streamlit.header("Fruityvice Fruit Advice!")
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("Please select a fruit to get information.")
+#   else:
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#     fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+#     streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#   streamlit.error()
+  
+  
+  
+ #create the repeatable code block (called a function)
+def get_fruityvice_data (this_fruit_choice):
+  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized=pd.json_normalilze(fruityvice_response.json())
+  return fruityvice_normalized
+#new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice=streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
+    streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 
-
+    
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
 
 
-fruit_choice_1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice_1)
+streamlit.header("The fuit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+# fruit_choice_1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+# streamlit.write('Thanks for adding ', fruit_choice_1)
+# #This will not work currectly, but just go with it for now
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
-#This will not work currectly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('new_fruit')")
+    return "Thanks for adding"+new_fruit
+add_my_fruit=streamlit.text_input("What fruit would you like to add?")
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function=insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
+
