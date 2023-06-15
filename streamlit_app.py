11baf18d602c136c611d9c424e565978913b2256
TyWang1990 <111158867+TyWang1990@users.noreply.github.com>
2023-06-15 20:23:34 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,20 +56,6 @@ try:
     streamlit.dataframe(back_from_function)
 
 
-    
-# don't run anything past here while we troubleshoot
-# streamlit.stop()
-
-
-
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_rows = my_cur.fetchall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_rows)
-
-
 
 streamlit.header('The fuit load list contains:')
 #snowflake-related functions
