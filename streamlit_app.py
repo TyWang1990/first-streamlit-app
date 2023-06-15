de79651e515aab0ecdaff7f0749b912661f369ad
TyWang1990 <111158867+TyWang1990@users.noreply.github.com>
2023-06-15 21:07:04 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,13 @@ def get_fruityvice_data (this_fruit_choice):
 
 
 streamlit.header('Fruityvice Fruit Advice!')
-try:
-   fruit_choice=streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    if back_from_function is not None:
+        streamlit.dataframe(back_from_function)
+    else:
+        streamlit.warning(f"No data found for {fruit_choice}.")
     
